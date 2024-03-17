d97b97ba09bf135a1789052aeb18e8cac8b46da2
Haider <161382569+21139hf@users.noreply.github.com>
2024-03-18 09:43:09 +1300

    Update game.js
    Face image

--- game.js
+++ game.js
@@ -17,31 +17,45 @@ function setup(){
     cnv = new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
     world.gravity.y = 80;
     
+    //Ground 
     platform_1 = new Sprite(SCREEN_WIDTH/2, SCREEN_HEIGHT, SCREEN_WIDTH, 5, 'k');
     platform_1.color = 'black'
     platform_1.bounciness = 0;
     platform_1.friction = 0;
     
+    // Platforms
+    platform_2 = new Sprite(0, SCREEN_HEIGHT/1.5, SCREEN_WIDTH/4, 5, 'k');
+    platform_2.color = 'black'
+    platform_2.bounciness = 0;
+    platform_2.friction = 0;
+    platform_3 = new Sprite(SCREEN_WIDTH, SCREEN_HEIGHT/2, SCREEN_WIDTH/4, 5, 'k');
+    platform_3.color = 'black'
+    platform_3.bounciness = 0;
+    platform_3.friction = 0;
     
+    //Walls
     wallLH  = new Sprite(0, height/2, 8, height, 'k');
     wallLH.color = 'black';
     wallLH.bounciness = 0;
     wallLH.friction = 0;
-    
     wallRH  = new Sprite(width, height/2, 8, height, 'k');
     wallRH.color = 'black';
     wallRH.bounciness = 0;
     wallRH.friction = 0;
-    
     wallTop = new Sprite(width/2, 0, width, 8, 'k');
     wallTop.color = 'black';
     wallTop.bounciness = 0;
     wallTop.friction = 0;
   
+    //Player
     player = new Sprite(100,  SCREEN_HEIGHT/2, 25, 25, 'd');
     player.bounciness = 0;
-    player.color = 'lightblue';
+    img = loadImage('/images/face.png');
+    player.addImage(img);
+
     
+
+    //Keyboard imputs
     document.addEventListener("keydown", function(event) {
         if (event.code === 'ArrowLeft') {
             player.vel.x = -5;
@@ -50,9 +64,9 @@ function setup(){
             player.vel.x = 5; 
         }
         else if (event.code === 'ArrowUp') {
-            player.vel.y = -20; 
+            player.vel.y = -25; 
         }
-});
+    });
 
     document.addEventListener("keyup", function(event) {
         if (event.code === 'ArrowLeft') {
@@ -63,6 +77,9 @@ function setup(){
             player.vel.x = 0; 
         }
     });
+    
+    
+    
 }
 
    
@@ -70,10 +87,10 @@ function setup(){
 // draw()
 /*******************************************************/
 function draw(){
-      background('grey')
-
-    
+    background('grey')
+    img.resize(25, 25);
 }
+
 
 function startScreen(){
 
